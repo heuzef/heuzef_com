bc1636f8218b5329ee8fd13ac58f208b37fb4b4c
Heuzef <contact@heuzef.com>
2025-07-11 16:31:10 +0200

    Add custom JS for Umami

---
+++ static/custom.js
@@ -0,0 +1,10 @@
+// static/js/custom.js
+
+// UMAMI
+window.addEventListener('DOMContentLoaded', (event) => {
+    var script = document.createElement('script');
+    script.defer = true;
+    script.src = "https://analytics.heuzef.com/script.js";
+    script.setAttribute('data-website-id', '6c118b82-fe22-4fa1-ba01-71e5535f8f36');
+    document.head.appendChild(script);
+});
