0ab6efcb0fc7827b2c0a08224fa7b3784af14421
Heuzef <contact@heuzef.com>
2025-07-11 13:28:33 +0200

    Update Deploy script

--- deploy_pelican.sh
+++ deploy_pelican.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Update, generate and publish the website with Pelican
+echo "Current user: $(whoami)"
+echo "Current directory: $(pwd)"
+echo "Python version: $(python --version)"
+echo "Pelican version: $(pelican --version)"
 cd ~/GIT/heuzef_com
 git pull
 make html
